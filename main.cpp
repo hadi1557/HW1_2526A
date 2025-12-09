a988db06222cf87dbcb04aa19e4b637eb88a86f8
hadi1557 <hadi@DESKTOP-50VRQDD.localdomain>
2025-12-09 15:08:59 +0200

    finish blockchain.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,80 @@
-//
-// Created by tmlk1 on 12/8/2025.
-//
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <fstream>
+
 #include "BlockChain.h"
+#include "Utilities.h"
 
+using std::ifstream;
+using std::ofstream;
+using std::string;
+using std::cout;
+using std::cerr;
+using std::endl;
 
-int main() {
-    BlockChain blockchain = BlockChainInit();
-    Transaction tr;
-    tr.value = 13;
-    tr.sender = "Sender";
-    tr.receiver = "Receiver";
-    BlockChainAppendTransaction(blockchain, 10,"hamodi", "Kayan","10Dec");
-    BlockChainAppendTransaction(blockchain, 2,"Kayan", "Marwan","11Dec");
-    BlockChainAppendTransaction(blockchain, 3,"Marwan", "Kayan","12Dec");
-     int size =       BlockChainGetSize(blockchain);
-    BlockChainAppendTransaction(blockchain,tr, "Test" );
-    BlockChainDestroy(blockchain);
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        cerr << getErrorMessage();
+        return 0;
+    }
 
+    const char* op = argv[1];
+    if (strcmp(op, "format") != 0 &&
+        strcmp(op, "hash") != 0 &&
+        strcmp(op, "compress") != 0 &&
+        strcmp(op, "verify") != 0) {
+        cerr << getErrorMessage();
+        return 0;
+    }
 
+    ifstream source(argv[2]);
+    if (!source) {
+        cerr << "Error opening/closing file for reading.\n";
+        return 1;
+    }
+
+    BlockChain blockChain = BlockChainLoad(source);
+
+    if (strcmp(op, "format") == 0) {
+        ofstream target(argv[3]);
+        if (!target) {
+            cerr << "Error opening/closing file for reading.\n";
+            BlockChainDestroy(blockChain);
+            return 1;
+        }
+        BlockChainDump(blockChain, target);
+    } else if (strcmp(op, "hash") == 0) {
+        ofstream target(argv[3]);
+        if (!target) {
+            cerr << "Error opening/closing file for reading.\n";
+            BlockChainDestroy(blockChain);
+            return 1;
+        }
+        BlockChainDumpHashed(blockChain, target);
+    } else if (strcmp(op, "compress") == 0) {
+        ofstream target(argv[3]);
+        if (!target) {
+            cerr << "Error opening/closing file for reading.\n";
+            BlockChainDestroy(blockChain);
+            return 1;
+        }
+        BlockChainCompress(blockChain);
+        BlockChainDump(blockChain, target);
+    } else if (strcmp(op, "verify") == 0) {
+        ifstream target(argv[3]);
+        if (!target) {
+            cerr << "Error opening/closing file for reading.\n";
+            BlockChainDestroy(blockChain);
+            return 1;
+        }
+        if (BlockChainVerifyFile(blockChain, target)) {
+            cout << "Verification passed\n";
+        } else {
+            cout << "Verification failed\n";
+        }
+    }
+
+    BlockChainDestroy(blockChain);
+    return 0;
 }
